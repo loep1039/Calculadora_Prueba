f3a7fc2343554fc11af45c4e954edc95086f1acf
loep1039 <213527042+loep1039@users.noreply.github.com>
2026-03-22 10:39:09 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,13 @@
-self.addEventListener('fetch', function(event) {
-  // Este código permite que la app cargue más rápido
+const CACHE_NAME = 'calc-v2'; // Cambiamos el nombre para forzar actualización
+
+self.addEventListener('install', (event) => {
+  self.skipWaiting(); // Obliga al nuevo Service Worker a activarse de inmediato
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(clients.claim()); // Toma el control de la página inmediatamente
+});
+
+self.addEventListener('fetch', (event) => {
   event.respondWith(fetch(event.request));
 });
